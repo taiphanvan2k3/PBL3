1793d316f243a0856336e8d8865445a532773ed6
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-02-09 14:53:46 +0700

    9/2/2023: test

--- PBL3/Form1.Designer.cs
+++ PBL3/Form1.Designer.cs
@@ -28,16 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.customButton1 = new WindowsFormsApp1.Controls.CustomButton();
-            this.toggleButton1 = new WindowsFormsApp1.Controls.ToggleButton();
-            this.boxCalendar1 = new WindowsFormsApp1.Controls.BoxCalendar();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -45,6 +53,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
