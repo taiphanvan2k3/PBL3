9d3ea469014c2d30ff8b1a71de901b450b509696
Dinh Manh <tranmanhproh1@gmail.com>
2023-02-10 07:20:14 +0700

    test

--- PBL3/GUI/Form01.Designer.cs
+++ PBL3/GUI/Form01.Designer.cs
@@ -31,6 +31,7 @@
             this.customButton1 = new PBL3.MyCustomControl.CustomButton();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // customButton1
@@ -54,9 +55,11 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(238, 112);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(277, 22);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "test";
             // 
             // textBox2
             // 
@@ -65,11 +68,21 @@
             this.textBox2.Size = new System.Drawing.Size(274, 22);
             this.textBox2.TabIndex = 2;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(605, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form01
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.customButton1);
@@ -85,5 +98,6 @@
         private MyCustomControl.CustomButton customButton1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
