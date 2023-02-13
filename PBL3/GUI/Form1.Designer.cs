1081d1527690ae97fcc84585ed0a86b641d53f59
Dinh Manh <tranmanhproh1@gmail.com>
2023-02-13 08:25:46 +0700

    13/2/2023

--- PBL3/GUI/Form1.Designer.cs
+++ PBL3/GUI/Form1.Designer.cs
@@ -31,9 +31,10 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.customButton1 = new PBL3.MyCustomControl.CustomButton();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.customButton1 = new PBL3.MyCustomControl.CustomButton();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -43,7 +44,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(277, 22);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "test";
+            this.textBox1.Text = "TAi khoan";
             // 
             // textBox2
             // 
@@ -51,6 +52,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(274, 22);
             this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "mAT KHAU";
             // 
             // button1
             // 
@@ -60,6 +62,24 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "MK";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(615, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // customButton1
             // 
@@ -79,36 +99,28 @@
             this.customButton1.TextColor = System.Drawing.Color.WhiteSmoke;
             this.customButton1.UseVisualStyleBackColor = false;
             // 
-            // label2
+            // label1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(106, 193);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "TK";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(615, 193);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // Form01
+            // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.customButton1);
-            this.Name = "Form01";
+            this.Name = "Form1";
             this.Text = "Form01";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -123,5 +135,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
