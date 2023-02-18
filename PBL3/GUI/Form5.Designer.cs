46be17a56335cc35c62bec45a8440ab952d0ac64
thanhNgan13 <nngann2402@gmail.com>
2023-02-18 10:44:24 +0700

    upp

--- PBL3/GUI/Form5.Designer.cs
+++ PBL3/GUI/Form5.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1133, 705);
             this.Controls.Add(this.label1);
             this.Name = "Form5";
             this.Text = "Form5";
