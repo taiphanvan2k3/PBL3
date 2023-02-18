bf053392a3db36b22480948113664a899594aba4
thanhNgan13 <nngann2402@gmail.com>
2023-02-18 10:50:13 +0700

    bg

--- PBL3/GUI/Form5.Designer.cs
+++ PBL3/GUI/Form5.Designer.cs
@@ -49,6 +49,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form5";
             this.Text = "Form5";
+            this.Load += new System.EventHandler(this.Form5_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
