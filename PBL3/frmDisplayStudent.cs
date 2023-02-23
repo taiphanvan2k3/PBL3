7f884b1bd44c1fc2ab2b9e76e3a17d2f91697c7c
thanhNgan13 <nngann2402@gmail.com>
2023-02-23 16:42:36 +0700

    Update for Ngan 23/2 - 16h52

---
+++ PBL3/frmDisplayStudent.cs
@@ -0,0 +1,44 @@
+﻿using PBL3.MyUserControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBL3
+{
+    public partial class frmDisplayStudent : Form
+    {
+        public frmDisplayStudent()
+        {
+            InitializeComponent();
+            UC_InfomationStudent uc = new UC_InfomationStudent();
+            addUserControl(uc);
+        }
+
+        private void addUserControl(UserControl userControl)
+        {
+            userControl.Dock = DockStyle.Fill;
+            ptnContainer.Controls.Clear();
+            ptnContainer.Controls.Add(userControl);
+            userControl.BringToFront();
+
+        }
+        private void btnInfomation_Click(object sender, EventArgs e)
+        {
+            UC_InfomationStudent uc = new UC_InfomationStudent();
+            addUserControl(uc);
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            UC_Class uc = new UC_Class();
+            addUserControl(uc);
+        }
+
+    }
+}
