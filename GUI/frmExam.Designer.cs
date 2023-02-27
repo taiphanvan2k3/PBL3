dbc92055c7f7ef1b72bebcabad9eb1acf64fe852
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-02-27 19:53:21 +0700

    27/2/2023: Tạo form exam

---
+++ GUI/frmExam.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace GUI
+{
+    partial class frmExam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelGradient1 = new GUI.MyCustomControl.PanelGradient();
+            this.SuspendLayout();
+            // 
+            // panelGradient1
+            // 
+            this.panelGradient1.BackColor = System.Drawing.Color.White;
+            this.panelGradient1.BorderRadius = 30;
+            this.panelGradient1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelGradient1.ForeColor = System.Drawing.Color.Black;
+            this.panelGradient1.GradientAngle = 90F;
+            this.panelGradient1.GradientBottomColor = System.Drawing.Color.CadetBlue;
+            this.panelGradient1.GradientTopColor = System.Drawing.Color.DodgerBlue;
+            this.panelGradient1.Location = new System.Drawing.Point(0, 0);
+            this.panelGradient1.Name = "panelGradient1";
+            this.panelGradient1.Size = new System.Drawing.Size(184, 450);
+            this.panelGradient1.TabIndex = 0;
+            // 
+            // frmExam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 450);
+            this.Controls.Add(this.panelGradient1);
+            this.Name = "frmExam";
+            this.Text = "frmExam";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyCustomControl.PanelGradient panelGradient1;
+    }
+}
