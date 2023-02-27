242c470e6190d46aac4df61606889e5370889a02
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-02-27 20:48:22 +0700

    27/2/2023: tạo form thi

--- GUI/frmExam.Designer.cs
+++ GUI/frmExam.Designer.cs
@@ -29,12 +29,19 @@
         private void InitializeComponent()
         {
             this.panelGradient1 = new GUI.MyCustomControl.PanelGradient();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelGradient1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelGradient1
             // 
             this.panelGradient1.BackColor = System.Drawing.Color.White;
-            this.panelGradient1.BorderRadius = 30;
+            this.panelGradient1.BorderRadius = 0;
+            this.panelGradient1.Controls.Add(this.panel2);
             this.panelGradient1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelGradient1.ForeColor = System.Drawing.Color.Black;
             this.panelGradient1.GradientAngle = 90F;
@@ -42,17 +49,58 @@
             this.panelGradient1.GradientTopColor = System.Drawing.Color.DodgerBlue;
             this.panelGradient1.Location = new System.Drawing.Point(0, 0);
             this.panelGradient1.Name = "panelGradient1";
-            this.panelGradient1.Size = new System.Drawing.Size(184, 450);
+            this.panelGradient1.Size = new System.Drawing.Size(200, 530);
             this.panelGradient1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(200, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(856, 530);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(164, 69);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Xin chào";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phan Văn Tài";
             // 
             // frmExam
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(874, 450);
+            this.ClientSize = new System.Drawing.Size(1056, 530);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.panelGradient1);
             this.Name = "frmExam";
             this.Text = "frmExam";
+            this.panelGradient1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -60,5 +108,9 @@
         #endregion
 
         private MyCustomControl.PanelGradient panelGradient1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
