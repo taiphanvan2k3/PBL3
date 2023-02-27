767d3170864278ae6b2fb9981318003f7e709d11
Dinh Manh <tranmanhproh1@gmail.com>
2023-02-27 20:43:49 +0700

    test

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -17,6 +17,7 @@ namespace GUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmDisplayStudent());
+            Application.Run(new Home());
         }
     }
 }
