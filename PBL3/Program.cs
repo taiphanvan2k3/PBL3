c4b3ec1e5196e7c0c5abcd6a65ca6592b06bee58
thanhNgan13 <nngann2402@gmail.com>
2023-02-27 21:51:47 +0700

    9h51

--- PBL3/Program.cs
+++ PBL3/Program.cs
@@ -17,7 +17,7 @@ namespace PBL3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin()); 
+            Application.Run(new Home()); 
         }
     }
 }
