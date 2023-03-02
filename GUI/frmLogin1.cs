1b8e7214cf496f4252759859cd68feab0e87e450
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-03-02 16:20:51 +0700

    2/3/2023 4:20PM thêm comment, làm di chuyển frmLogin1

--- GUI/frmLogin1.cs
+++ GUI/frmLogin1.cs
@@ -1,5 +1,6 @@
 ﻿using BLL;
 using Guna.UI2.WinForms.Enums;
+using Org.BouncyCastle.Math.Field;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,8 +19,8 @@ namespace GUI
     {
         public frmLogin1()
         {
-
             InitializeComponent();
+            //2 button nằm cùng 1 vị trí nhưng ban đầu cho buttonDoNotShow lên trước nên mới dùng BringToFont 
             btnDoNotShow.BringToFront();
         }
 
@@ -55,6 +56,9 @@ namespace GUI
 
         private void txtUsername_Click(object sender, EventArgs e)
         {
+            //Edit:2/3/2023 by Tai
+            //Khi bấm vào lại textbox username thì ẩn mật khẩu đi
+            //và hiện tại btnShow nằm chồng lên btnDoNotShow nên phải mang nó lên trước 
             txtPassword.PasswordChar = true;
             btnDoNotShow.BringToFront();
         }
@@ -95,6 +99,33 @@ namespace GUI
             }
         }
 
+        #region các thuộc tính xử lí form di chuyển
+        private bool move;
+        private int moveX, moveY;
+        #endregion
+        private void panelTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Chỉ xử lí việc di chuyển form khi nhấn chuột trái
+            if (e.Button == MouseButtons.Left)
+            {
+                move = true;
+                moveX = e.X;
+                moveY = e.Y;
+            }
+        }
 
+        private void panelTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (move)
+            {
+                //set lại vị trí của form trên màn hình
+                this.SetDesktopLocation(MousePosition.X - moveX, MousePosition.Y - moveY);
+            }   
+        }
+
+        private void panelTitle_MouseUp(object sender, MouseEventArgs e)
+        {
+            move = false;
+        }
     }
 }
