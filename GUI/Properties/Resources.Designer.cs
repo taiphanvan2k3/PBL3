b71d0a4262df6c1bd25bce0d0f0d7e6d1eda2628
Dinh Manh <tranmanhproh1@gmail.com>
2023-04-08 08:41:48 +0700

    8/4/2023 DinhManh handle conflictic

--- GUI/Properties/Resources.Designer.cs
+++ GUI/Properties/Resources.Designer.cs
@@ -300,28 +300,5 @@ namespace GUI.Properties {
             }
         }
 
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap calendar_date
-        {
-            get
-            {
-                object obj = ResourceManager.GetObject("calendar_date", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap calendar_day
-        {
-            get
-            {
-                object obj = ResourceManager.GetObject("calendar_day", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }   
