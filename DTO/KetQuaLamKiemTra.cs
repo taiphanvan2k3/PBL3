2b0aeafbfc70894e3f652716b6112f2840327f30
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-04-11 22:40:15 +0700

    11/4/2023: 10:39 PM thêm QuanLyDiem_DAL

---
+++ DTO/KetQuaLamKiemTra.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class KetQuaLamKiemTra
+    {
+        [DisplayName("Mã học phần")]
+        public string MaHP { get; set; }
+
+        [DisplayName("Tên môn học")]
+        public string TenMH { get; set; }
+
+        [DisplayName("Loại bài")] //BT,GK,CK
+        public string LoaiBaiKiemTra { get; set; }
+
+        [DisplayName("Điểm số")]
+        public double Diem { get; set; }
+    }
+}
