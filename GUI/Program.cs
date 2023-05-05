b3ec69987c66fa1740636358da015b1e5150089f
Dinh Manh <tranmanhproh1@gmail.com>
2023-05-05 10:27:43 +0700

    5/5/2023 DinhManh

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -14,7 +14,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmDesignLogin());
+            Application.Run(new frmAdmin());
         }
     }
 }
