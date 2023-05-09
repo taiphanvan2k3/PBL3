7be3ddcf80769d57362dce9d97a5b038c8ef546a
Dinh Manh <tranmanhproh1@gmail.com>
2023-05-09 20:33:15 +0700

    Dinh Manh

--- GUI/frmExam.Designer.cs
+++ GUI/frmExam.Designer.cs
@@ -366,7 +366,7 @@
             this.ClientSize = new System.Drawing.Size(1763, 884);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(1300, 712);
+            this.MinimumSize = new System.Drawing.Size(1781, 931);
             this.Name = "frmExam";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmExam";
