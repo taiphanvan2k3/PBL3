6a2340f73ae8108454880cfd8af636ff6df5c196
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-06-09 09:23:18 +0700

    feat: push tạm thay đổi class CauHoi_DTO

--- DTO/CauHoi_DTO.cs
+++ DTO/CauHoi_DTO.cs
@@ -8,13 +8,20 @@ namespace DTO
         public string TenCauHoi { get; set; }
         public bool Checked { get; set; } = false;
 
-        //List này sẽ lưu các chuỗi đáp án sau khi đã xáo trộng ABCD và dùng để hiển thị lên giao diện 
+        //List này sẽ lưu các chuỗi đáp án sau khi đã xáo trộn ABCD và dùng để hiển thị lên giao diện 
         public List<string> DapAnHienThi;
 
         private List<string> DapAnDung;
 
+        //Lưu lại chỉ số của các đáp án đã chọn
         public List<int> IndexSelection = new List<int>();
         public bool IsSingleQuestion { get; set; }
+
+        #region edit 9/6/2023
+        private bool[] indexCorrectAnswer = new bool[] { false, false, false, false };
+        private string maDapAnDung = "";
+        private string maDapAnChon = "";
+        #endregion
 
         public CauHoi_DTO(string TenCauHoi, string DapAnA, string DapAnB, string DapAnC, string DapAnD,
                           string MaDapAnDung)
@@ -28,6 +35,7 @@ namespace DTO
                 DapAnC,
                 DapAnD
             };
+
             IsSingleQuestion = MaDapAnDung.Length == 1;
 
             //Thực hiện việc xác định đáp án đúng trước khi xoá trộn mảng hiển thị
@@ -45,7 +53,7 @@ namespace DTO
             foreach (char c in MaDapAnDung)
             {
                 int idx = char.ToUpper(c) - 'A';
-                
+                indexCorrectAnswer[idx] = true;
                 //Lưu lại đáp án đúng dưới dạng string để lát so sánh với đáp án người dùng chọn
                 DapAnDung.Add(DapAnHienThi[idx]);
             }
@@ -63,29 +71,46 @@ namespace DTO
                 string tmp = DapAnHienThi[i];
                 DapAnHienThi[i] = DapAnHienThi[j];
                 DapAnHienThi[j] = tmp;
+
+                // Khi xáo trộn đáp án hiển thị thì đồng thời phải xáo trộn đáp án đúng
+                bool temp = indexCorrectAnswer[i];
+                indexCorrectAnswer[i] = indexCorrectAnswer[j];
+                indexCorrectAnswer[j] = temp;
+            }
+
+            for (int i = 0; i < indexCorrectAnswer.Length; i++)
+            {
+                if (indexCorrectAnswer[i])
+                    maDapAnDung += ('A' + i);
             }
         }
 
         public bool CheckCorrectAnswer()
         {
-            List<string> DapAnChon = new List<string>();
-            foreach (int idx in IndexSelection)
-                DapAnChon.Add(DapAnHienThi[idx]);
+            //List<string> DapAnChon = new List<string>();
+            //foreach (int idx in IndexSelection)
+            //{
+            //    DapAnChon.Add(DapAnHienThi[idx]);
+            //    _indexSelectionAnswer += ('a' + idx);
+            //}
 
-            if (DapAnChon.Count != DapAnDung.Count)
-                return false;
+            //if (DapAnChon.Count != DapAnDung.Count)
+            //    return false;
 
-            //Do đáp án hiển thị bị xoá trộn lên nên giờ để check được làm đúng hay sai
-            //phải sort 2 mảng này rồi so sánh
-            DapAnChon.Sort();
-            DapAnDung.Sort();
+            ////Do đáp án hiển thị bị xoá trộn lên nên giờ để check được làm đúng hay sai
+            ////phải sort 2 mảng này rồi so sánh
+            //DapAnChon.Sort();
+            //DapAnDung.Sort();
 
-            for (int i = 0; i < DapAnDung.Count; i++)
-            {
-                if (DapAnChon[i] != DapAnDung[i])
-                    return false;
-            }
-            return true;
+            //for (int i = 0; i < DapAnDung.Count; i++)
+            //{
+            //    if (DapAnChon[i] != DapAnDung[i])
+            //        return false;
+            //}
+            //return true;
+            foreach(int idx in IndexSelection)
+                maDapAnChon += ('A' + idx);
+            return maDapAnDung == maDapAnChon;
         }
     }
 }
